bef4f9876bee6021b3733d9be651b7bb810dbfff
Veletronic <31300165+Veletronic@users.noreply.github.com>
2017-11-07 10:26:20 +0800

    Add files via upload

---
+++ ch9additional.py
@@ -0,0 +1,76 @@
+edmund = ("Edmund", "Jonathan", "*REDACTED*", 0000 , "Student", "United Kingdom, London")
+(firstname, secondname, lastname, birthyear, occupation, birthplace) = edmund
+Other  = [
+    ("Vassili", "Dmitri", 1969, ["Engineer", "Russia, Moscow"]),
+    ("Tasha", "Matthews", 1997, ["Scientist", "USA, Los Angelas"]),
+    ("Ruben", "Pantoja", 1998, ["Unemployed", "USA, Mexico"]),
+    ("Jason", "Vorhees", 0000, ["N/A", "N/A"])]
+
+other = print ("Please capitilise the 'o', i'm too lazy to copy and paste previously written code.")
+
+errortxt = ("CH....CH......CH...AH....AH....AH....#@$$#@$@&#@%*@@%**%&@%*@%&*@%*#!!%#%!%!*%!")
+
+
+vassili = ( ("Vassili", "Dmitri"), (19, "February", 1969),
+                    "Computer Engineering", ("Russia, Moscow"),
+                    [ ("Arrested for loitering by the music theatre, 1980"),
+                    ("Studied quantum computing at the university of oxford, 1985"),
+                    ("Awarded for being the first russian man without a russian accent, 1990") ])
+
+tasha = ( ("Tasha", "Matthews"), (21, "December", 1997),
+          "Chemcial Engineering", ("USA, Los Angelas"),
+          [ ("Obtained a speeding ticket whilst she was driving the wheel with her feet, 2015"),
+            ("Enjoys producing books of a fantasy genre in here free time"),
+            ("Awarded for winning a spelling bee, 2000") ])
+
+ruben = ( ("Ruben", "Pantoja"), (10, "February", 1998),
+          "Unemployed", ("USA, Mexico"),
+          [ ("No further info available :(") ])
+
+
+    
+choices = ("""
+List of commands:
+    1.Other --> Learn about random (fictional) people
+    2.Type in the names of these other people to learn more about them
+    3.Type in these commands to learn more about me, the programmer:
+        I.firstname
+        II.Secondname
+        III.lastname
+        IV.birthyear
+        V.occupation
+        VI.birthplace""")
+print (choices)
+
+jason = ( (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt),
+          (errortxt) ) 
+          
+
